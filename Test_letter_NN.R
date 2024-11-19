b73388465aba808d374758722d51c5209fea6f55
Sara Antonijevic <saraant@tamu.edu>
2024-11-19 10:08:16 -0600

    Test against other enhanced versions of the code to see which one provides accurate results.

--- Test_letter_NN.R
+++ Test_letter_NN.R
@@ -21,7 +21,8 @@ Xt <- as.matrix(letter_test[, -1])
 source("FunctionsNN.R")
 
 # [ToDo] Source the functions from HW3 (replace FunctionsLR.R with your working code)
-source("FunctionsLR.R")
+#source("FunctionsLR.R")
+source("letsSee.R")
 
 # Recall the results of linear classifier from HW3
 # Add intercept column
@@ -42,9 +43,11 @@ out2 = NN_train(Xtrain, Ytrain, Xval, Yval, lambda = 0.001,
 plot(1:length(out2$error), out2$error, ylim = c(0, 70))
 lines(1:length(out2$error_val), out2$error_val, col = "red")
 
+
 # Evaluate error on testing data
 test_error = evaluate_error(Xt, Yt, out2$params$W1, out2$params$b1, out2$params$W2, out2$params$b2)
 test_error # 16.1
+
 
 # [ToDo] Try changing the parameters above to obtain a better performance,
 # this will likely take several trials
